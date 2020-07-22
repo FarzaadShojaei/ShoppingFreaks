8544180b2506250dfe713f7550f11477e2162661
MohammadMoeinKeyvani <moeink1e@gmail.com>
2020-07-22 16:24:42 +0430

    ProductEntity V0.1

--- Final Java Project 98-2/src/model/entity/ProductEntity.java
+++ Final Java Project 98-2/src/model/entity/ProductEntity.java
@@ -15,4 +15,142 @@ public class ProductEntity {
     private int numberOfPlate;
     private int numberOfTape;
     private int numberOfAxe;
+    private int numberOfBat;
+
+    //////////////////////////////////////////////// Setters
+    public ProductEntity setNumberOfTankTop(int numberOfTankTop) {
+        this.numberOfTankTop = numberOfTankTop;
+        return this;
+    }
+
+    public ProductEntity setNumberOfJeans(int numberOfJeans) {
+        this.numberOfJeans = numberOfJeans;
+        return this;
+    }
+
+    public ProductEntity setNumberOfShirt(int numberOfShirt) {
+        this.numberOfShirt = numberOfShirt;
+        return this;
+    }
+
+    public ProductEntity setNumberOfPizza(int numberOfPizza) {
+        this.numberOfPizza = numberOfPizza;
+        return this;
+    }
+
+    public ProductEntity setNumberOfHotDog(int numberOfHotDog) {
+        this.numberOfHotDog = numberOfHotDog;
+        return this;
+    }
+
+    public ProductEntity setNumberOfTurkishKebab(int numberOfTurkishKebab) {
+        this.numberOfTurkishKebab = numberOfTurkishKebab;
+        return this;
+    }
+
+    public ProductEntity setNumberOfLittlePrince(int numberOfLittlePrince) {
+        this.numberOfLittlePrince = numberOfLittlePrince;
+        return this;
+    }
+
+    public ProductEntity setNumberOfWarAndPeace(int numberOfWarAndPeace) {
+        this.numberOfWarAndPeace = numberOfWarAndPeace;
+        return this;
+    }
+
+    public ProductEntity setNumberOfMetro2034(int numberOfMetro2034) {
+        this.numberOfMetro2034 = numberOfMetro2034;
+        return this;
+    }
+
+    public ProductEntity setNumberOfFork(int numberOfFork) {
+        this.numberOfFork = numberOfFork;
+        return this;
+    }
+
+    public ProductEntity setNumberOfOven(int numberOfOven) {
+        this.numberOfOven = numberOfOven;
+        return this;
+    }
+
+    public ProductEntity setNumberOfPlate(int numberOfPlate) {
+        this.numberOfPlate = numberOfPlate;
+        return this;
+    }
+
+    public ProductEntity setNumberOfTape(int numberOfTape) {
+        this.numberOfTape = numberOfTape;
+        return this;
+    }
+
+    public ProductEntity setNumberOfAxe(int numberOfAxe) {
+        this.numberOfAxe = numberOfAxe;
+        return this;
+    }
+
+    public ProductEntity setNumberOfBat(int numberOfBat) {
+        this.numberOfBat = numberOfBat;
+        return this;
+    }
+
+    //////////////////////////////////////////////// Getters
+    public int getNumberOfTankTop() {
+        return numberOfTankTop;
+    }
+
+    public int getNumberOfJeans() {
+        return numberOfJeans;
+    }
+
+    public int getNumberOfShirt() {
+        return numberOfShirt;
+    }
+
+    public int getNumberOfPizza() {
+        return numberOfPizza;
+    }
+
+    public int getNumberOfHotDog() {
+        return numberOfHotDog;
+    }
+
+    public int getNumberOfTurkishKebab() {
+        return numberOfTurkishKebab;
+    }
+
+    public int getNumberOfLittlePrince() {
+        return numberOfLittlePrince;
+    }
+
+    public int getNumberOfWarAndPeace() {
+        return numberOfWarAndPeace;
+    }
+
+    public int getNumberOfMetro2034() {
+        return numberOfMetro2034;
+    }
+
+    public int getNumberOfFork() {
+        return numberOfFork;
+    }
+
+    public int getNumberOfOven() {
+        return numberOfOven;
+    }
+
+    public int getNumberOfPlate() {
+        return numberOfPlate;
+    }
+
+    public int getNumberOfTape() {
+        return numberOfTape;
+    }
+
+    public int getNumberOfAxe() {
+        return numberOfAxe;
+    }
+
+    public int getNumberOfBat() {
+        return numberOfBat;
+    }
 }
